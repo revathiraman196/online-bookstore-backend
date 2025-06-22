179d0cdf6f52a3018e9f02f87fd76eedb1327765
revathi <revathiraman196@gmail.com>
2025-06-22 02:02:00 +0200

    DataBase_connection_setup

--- src/test/java/com/bnppf/kata/online_book_store/OnlineBookStoreApplicationTests.java
+++ src/test/java/com/bnppf/kata/online_book_store/OnlineBookStoreApplicationTests.java
@@ -1,13 +1,30 @@
 package com.bnppf.kata.online_book_store;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@Slf4j
 @SpringBootTest
 class OnlineBookStoreApplicationTests {
 
+	@Autowired
+	private DataSource dataSource;
+
 	@Test
-	void contextLoads() {
+	void testDBConnection() throws SQLException {
+		try (Connection connection=dataSource.getConnection()){
+			assertNotNull(connection);
+			log.info("Database connection established");
+		}
 	}
+
 
 }
