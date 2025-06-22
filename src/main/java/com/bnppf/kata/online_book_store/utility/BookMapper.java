1e8fda7fba180860cdf726c3f00f450cf358d137
revathi <revathiraman196@gmail.com>
2025-06-22 12:08:00 +0200

    Improvising_MapperwithMapStruct

--- src/main/java/com/bnppf/kata/online_book_store/utility/BookMapper.java
+++ src/main/java/com/bnppf/kata/online_book_store/utility/BookMapper.java
@@ -2,17 +2,13 @@ package com.bnppf.kata.online_book_store.utility;
 
 import com.bnppf.kata.online_book_store.dto.BookDTO;
 import com.bnppf.kata.online_book_store.entity.Book;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 
-@Component
-public class BookMapper {
-    // Convert Book entity to BookDTO
-    public BookDTO toDTO(Book book) {
-        return new BookDTO(book.getId(), book.getTitle(), book.getAuthor(), book.getPrice());
-    }
+@Mapper(componentModel = "spring")
+public interface BookMapper {
 
-    // Convert BookDTO to Book entity
-    public Book toEntity(BookDTO bookDTO) {
-        return new Book(bookDTO.getId(), bookDTO.getTitle(), bookDTO.getAuthor(), bookDTO.getPrice());
-    }
+    // Entity to Dto
+    BookDTO toDTO(Book book);
+    //Dto to Entity
+    Book toEntity(BookDTO bookDTO);
 }
